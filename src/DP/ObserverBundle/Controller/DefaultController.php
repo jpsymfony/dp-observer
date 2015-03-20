42839f35311302988fda4228cd663388fafeaa49
johnny3 <jp.saulnier11@gmail.com>
2015-03-20 19:22:35 +0100

    added implementation of observer dp

--- src/DP/ObserverBundle/Controller/DefaultController.php
+++ src/DP/ObserverBundle/Controller/DefaultController.php
@@ -5,15 +5,28 @@ namespace DP\ObserverBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use \DP\ObserverBundle\Entity\Sujet\DonneesMeteo;
+use DP\ObserverBundle\Entity\Observateur\AffichageConditions;
 
 class DefaultController extends Controller
 {
+
     /**
      * @Route("/", name="observer")
      * @Template()
      */
     public function indexAction()
     {
-        return array();
+        $donneesMeteo        = new DonneesMeteo(); // sujet, celui qui est observé
+        $affichageConditions = new AffichageConditions($donneesMeteo); // l'observateur, à qui on passe l'observé en argument 
+        // plutôt que d'appeler la fonction $affichageConditions->enregistrerObservateur($donnesMeteo)
+    
+        $donneesMeteo->setMesures(25, 10, 1200); // le sujet met à jour ses données
+        
+        $affichageConditions->actualiser($donneesMeteo); // l'observateur actualise ses données en fonction de ce que lui transmet le sujet
+        
+        return array('affichageConditions' => $affichageConditions);
+        
     }
+
 }
